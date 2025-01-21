05622265f5f84860ded31e336904bd92d2bda7b0
kimkimsun <mojung2205@naver.com>
2025-01-21 15:51:06 +0900

    wsclient modify

--- Assets/Scripts/WsClient.cs
+++ Assets/Scripts/WsClient.cs
@@ -8,18 +8,16 @@ public class WsClient : MonoBehaviour
         ws = new WebSocket("ws://localhost:7777");
         //서버에서 설정한 포트를 넣어줍니다.
 
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.Log("연결완료");
+        };
 
         ws.Connect();
+
+
         //연결합니다.
         ws.OnMessage += Call;
-        //이벤트 추가
-        /*
-         위랑 같은 것
-        ws.OnMessage += (sender, e) =>
-        {
-            Debug.Log("주소 :  "+((WebSocket)sender).Url+", 데이터 : "+e.Data);
-        };
-         */
     }
 
     void Call(object sender, MessageEventArgs e)
