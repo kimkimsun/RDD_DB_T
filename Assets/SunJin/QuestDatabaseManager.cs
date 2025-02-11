80a111164636d15b5d789f5efe9926f31056f8ec
kimkimsun <mojung2205@naver.com>
2025-02-11 16:52:37 +0900

    QDBM에 selectfrom 모든 함수 세팅

--- Assets/SunJin/QuestDatabaseManager.cs
+++ Assets/SunJin/QuestDatabaseManager.cs
@@ -44,16 +44,6 @@ public class QuestDatabaseManager : MonoBehaviour
         serverData = JsonUtility.FromJson<QuestResponse>(e.Data);
         Debug.Log(serverData.data[1].quest_code);
     }
-
-    public static void SelectFromTable(int quest_code,int selectData)
-    {
-        foreach(QuestData data in serverData.data)
-        {
-            if (data.quest_code == quest_code)
-                Debug.Log(data.quest_code);
-        }
-    }
-
     public static void SendUpdateNpcCode(int questCode, int newNpcCode)
     {
         var message = new UpdateDatabase
@@ -67,6 +57,88 @@ public class QuestDatabaseManager : MonoBehaviour
         Debug.Log("보낼 메시지: " + jsonMessage); // 메시지가 제대로 생성되는지 확인
         ws.Send(jsonMessage);
     }
+
+    #region SearchDB
+    public static void SelectQuestCode(int quest_code)
+    {
+        foreach(QuestData data in serverData.data)
+        {
+            if (data.quest_code == quest_code) { }
+        }
+    }
+
+    public static void SelectQuestName(string quest_name)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_name == quest_name) { }
+        }
+    }
+
+    public static void SelectQuestGetBallonAppears(bool quest_get_ballon_appears)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_get_ballon_appears == quest_get_ballon_appears) { }
+        }
+    }
+
+    public static void SelectQuestGetCondition(bool quest_get_condition)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_get_condition == quest_get_condition) { }
+        }
+    }
+
+    public static void SelectQuestGet(bool quest_get)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_get == quest_get) { }
+        }
+    }
+
+    public static void SelectQuestCompletionBallonAppears(bool quest_completion_ballon_appears)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_completion_ballon_appears == quest_completion_ballon_appears) { }
+        }
+    }
+
+    public static void SelectQuestCompletionCondition(bool quest_completion_condition)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_completion_condition == quest_completion_condition) { }
+        }
+    }
+
+    public static void SelectQuestCompletion(bool quest_completion)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_completion == quest_completion) { }
+        }
+    }
+
+    public static void SelectQuestProgress(string quest_progress)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_progress == quest_progress) { }
+        }
+    }
+
+    public static void SelectQuestDetails(string[] quest_details)
+    {
+        foreach (QuestData data in serverData.data)
+        {
+            if (data.quest_details == quest_details) { }
+        }
+    }
+    #endregion
 
     private void Update()
     {
