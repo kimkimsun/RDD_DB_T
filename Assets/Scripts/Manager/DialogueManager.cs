c80b85130cdbc9fd69c8a42c947a47551299ec0c
ChangDae1993 <jsj939639@gmail.com>
2025-02-13 17:00:03 +0900

    QM 데이터 로드08
    Dialogue CSV 재 작성 시작02

--- Assets/Scripts/Manager/DialogueManager.cs
+++ Assets/Scripts/Manager/DialogueManager.cs
@@ -34,12 +34,32 @@ public class DialogueManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DialogueGetter();
     }
 
+    string[,] tables;
+    public int lineSize;
+    public int rowSize;
+
+    [System.Serializable]
+    public class Dialogue
+    {
+        public string NPC_name;
+        public string[] dialogues;
+    }
+    public Dialogue[] dialogue;
     public void DialogueGetter()
     {
         string currentText = dialogueCSV.text.Substring(0, dialogueCSV.text.Length - 1);
+        string[] line = currentText.Split(new char[] { '\n' });
+        lineSize = line.Length;
+        rowSize = line[1].Split(new char[] { ',' }).Length;
+        tables = new string[lineSize, rowSize];
+
+        for(int i = 0; i <  lineSize; i++)
+        {
+            dialogue[i].NPC_name = line[i];
+        }
     }
 
     // Update is called once per frame
